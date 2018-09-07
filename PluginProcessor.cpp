ebe656cf06a40ab7935eabd26cefb69347e8926e
Andrew Haukness <ahauknes@ucsc.edu>
2018-09-06 18:57:02 -0700

    know works now

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -24,6 +24,9 @@ DistortionVstbyMeAudioProcessor::DistortionVstbyMeAudioProcessor()
                        )
 #endif
 {
+
+	state = new AudioProcessorValueTreeState(*this, nullptr);
+
 }
 
 DistortionVstbyMeAudioProcessor::~DistortionVstbyMeAudioProcessor()
